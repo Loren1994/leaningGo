5d62adeb464597bb60cca99e935756ce88c77c07
loren1994 <dayan805@163.com>
2019-06-26 08:25:11 +0800

    learn05

--- learn04.go
+++ learn04.go
@@ -28,12 +28,14 @@ func main() {
 		}
 	}
 OutLoop:
-	fmt.Println(">>>OutLoop")
-	for i := 1; i <= 9; i++ {
-		for j := 1; j <= i; j++ {
-			fmt.Printf(" %d x %d = %d ", i, j, i*j)
+	{
+		fmt.Println(">>>OutLoop")
+		for i := 1; i <= 9; i++ {
+			for j := 1; j <= i; j++ {
+				fmt.Printf(" %d x %d = %d ", i, j, i*j)
+			}
+			fmt.Println()
 		}
-		fmt.Println()
 	}
 	//4
 	for k, v := range []int{1, 2, 3, 4} {
@@ -54,6 +56,21 @@ OutLoop:
 	switch {
 	case len(a) > 0 && len(a) < 8:
 		fmt.Println(3)
+	}
+	//7
+	var t interface{}
+	t = &sum //true//"this is string"
+	switch t := t.(type) {
+	default:
+		fmt.Printf("unexpected type %T", t)
+	case bool:
+		fmt.Printf("type: %t\n", t)
+	case string:
+		fmt.Printf("type: %T\n", t)
+	case *int:
+		fmt.Printf("type: %T\n", t) // t 的类型是 int
+	case *bool:
+		fmt.Printf("type: %t\n", *t) // t 的类型是 *bool
 	}
 
 }
---
+++ learn05.go
@@ -0,0 +1,72 @@
+package main
+
+import "fmt"
+
+type Data struct {
+	complax  []int
+	instance InnerData
+	ptr      *InnerData
+}
+
+type InnerData struct {
+	a int
+}
+
+func passByValue(inFunc Data) Data {
+	fmt.Printf("%+v - %p\n", inFunc, &inFunc)
+	return inFunc
+}
+
+func main() {
+	//1
+	i, s := test(6, 6)
+	fmt.Printf("%v %v\n", i, s)
+	err, suc := test1(1, 1)
+	fmt.Printf("%v %v\n", err, suc)
+	err2, suc2 := test2(1, 1)
+	fmt.Printf("%v %v\n", err2, suc2)
+	//2
+	d, h, m := convertTime(18000)
+	fmt.Printf("%v - %v - %v\n", d, h, m)
+	//3
+	in := Data{
+		complax:  []int{1, 2, 3},
+		instance: InnerData{5},
+		ptr:      &InnerData{1},
+	}
+	// 输入结构的成员情况
+	fmt.Printf("in value: %+v\n", in)
+	// 输入结构的指针地址
+	fmt.Printf("in ptr: %p\n", &in)
+	// 传入结构体，返回同类型的结构体
+	out := passByValue(in)
+	// 输出结构的成员情况
+	fmt.Printf("out value: %+v\n", out)
+	// 输出结构的指针地址
+	fmt.Printf("out ptr: %p\n", &out)
+	// 结论:
+	// 所有的 Data 结构的指针地址发生了变化，意味着所有的结构都是一块新的内存，
+	// 无论是将 Data 结构传入函数内部，还是通过函数返回值传回 Data 都会发生复制行为。
+	// 所有的 Data 结构中的成员值都没有发生变化，原样传递，意味着所有参数都是值传递。
+	// Data 结构的 ptr 成员在传递过程中保持一致，
+	// 表示指针在函数参数值传递中传递的只是指针值，不会复制指针指向的部分。
+}
+
+func convertTime(seconds int) (day int, hour int, minute int) {
+	day = seconds / (60 * 60 * 24)
+	hour = seconds / (60 * 60)
+	minute = seconds / 60
+	return
+}
+
+func test(a, b int) (string, int) {
+	return "print:", a + b
+}
+
+func test1(a, b int) (err string, success int) {
+	return "sss", 1
+}
+
+func test2(a, b int) (err, success int) {
+	return 1, 1
+}
